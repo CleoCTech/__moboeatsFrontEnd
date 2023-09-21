4d01a151189b963782beb445af5046512c325288
Kings-Israel <47811074+Kings-Israel@users.noreply.github.com>
2023-09-21 08:17:31 +0300

    UPdate

---
+++ dist/assets/ForgotPassword-d89c2357.js
@@ -0,0 +1,1 @@
+import{_ as e,b as c,c as o}from"./index-ef88ba51.js";const r={};function t(s,n){return c(),o("div")}const _=e(r,[["render",t]]);export{_ as default};
---
+++ dist/assets/ResetPassword-6d889ed1.js
@@ -0,0 +1,1 @@
+import{_ as e,b as c,c as r}from"./index-ef88ba51.js";const s={};function t(o,n){return c(),r("div")}const _=e(s,[["render",t]]);export{_ as default};
